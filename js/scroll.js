b27670692da9e0fea0fef88688ec8f77c5871698
Leo Hetsch <leo.hetsch@gmail.com>
2013-09-17 19:09:31 +0200

    REAL infinite scroll

--- js/scroll.js
+++ js/scroll.js
@@ -4,11 +4,32 @@ $(function(){
         var last = $('.last-item');
         // console.log($('body').scrollTop() + ' / ' + last.offset().top);
 
-        if ($('body').scrollTop() + 2000 >= last.offset().top && API_URL) {
+        if ($('body').scrollTop() + 2000 >= last.offset().top ) {
 
-            gettingPics();
+        	// il reste des images à charger
+        	if (API_URL) {
+        		gettingPics();
+            	
+        	}
+            
+            // sinon on répète à l'infini et au delà
+            else {
+            	items = $('.g .contener');
+            	var length = items.length;
+            	items.each(function(i){
+
+            		copy = $(this).clone();
+            		copy.appendTo('.g');
+
+            		// last item
+            		if (i+1 == length) {
+            			LAST_ITEM = i+1;
+            			copy.addClass('last-item');
+            		}
+            	});
+            }
+
             last.removeClass('last-item');
-
         }
     });
 })
