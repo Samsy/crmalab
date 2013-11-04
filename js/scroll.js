ca58ca6b1650f52967130c82fc93bbb2d35bd2e3
Clément Bardon <clement@MacBook-Air-de-Clement.local>
2013-11-04 11:58:55 +0100

    repass on push to refresh

--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,34 @@
 var LENGTH = 0;
 
 $(function(){
+    var vanishedLogo = false;
     $(document).scroll(function(e){
-
+        var positionScroll = $(window).scrollTop();
         // Little parallax effect on the logo
-        var scrollOffset = $(document).scrollTop();
-        $('.header').css('top',(-scrollOffset*1.5));
+        // var scrollOffset = $(document).scrollTop();
+        if(!vanishedLogo && positionScroll > 0 && loaded){
+            $('.header').animate({opacity:0}, 500, function(){
+                vanishedLogo = true;
+            });
+        }
+        if(positionScroll === 0 && vanishedLogo){
+            $('.header').animate({opacity:1}, 1, function(){
+                vanishedLogo = false;
+            });
+        }
+        //css('top',(-scrollOffset*1.5));
 
         var last = $('.last-item');
         // console.log($('body').scrollTop() + ' / ' + last.offset().top);
+        if(last.length){
+            if ($('body').scrollTop() + 2000 >= last.offset().top ) {
+                // il reste des images à charger
+                if (API_URL) {
+                    gettingPics();
+                }
+                last.removeClass('last-item');
+            }
 
-        if ($('body').scrollTop() + 2000 >= last.offset().top ) {
-
-        	// il reste des images à charger
-        	if (API_URL) {
-        		gettingPics();
-            	
-        	}
-            
-
-            last.removeClass('last-item');
         }
     });
-})
+});
